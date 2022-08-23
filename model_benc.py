ad66a6dcdff85ce1e249d4dfab656d574ae8c69e
XiaobingSuper <xiaobing.zhang@intel.com>
2022-08-22 22:59:03 -0400

    add benchmark test case

---
+++ model_benc.py
@@ -0,0 +1,64 @@
+import torch
+import torchvision
+#import intel_extension_for_pytorch as ipex
+import copy
+import time
+from utils_vis import make_dot, draw
+import torch.profiler as profiler
+
+torch.manual_seed(2020)
+
+#model = torchvision.models.resnet50()
+
+#model =  torchvision.models.shufflenet_v2_x2_0()
+
+model = torchvision.models.mobilenet_v3_small()
+model = model.to(memory_format=torch.channels_last).eval()
+
+
+batch_size = 1
+x = torch.randn(batch_size, 3, 224, 224).to(memory_format=torch.channels_last)
+
+with torch.no_grad():
+    traced_model = torch.jit.trace(model, x)
+    traced_model = torch.jit.freeze(traced_model).eval()
+
+warm_up = 200
+
+with torch.no_grad():
+    for i in range(warm_up):
+        y = traced_model(x)
+    graph = traced_model.graph_for(x)
+    #print(graph)
+    #draw(graph).render('renet50_ipex')
+    #draw(graph).render('shufflenet_v2_x2_0_ipex')
+    #draw(graph).render('mobilenet_v3_small_ipex')
+
+print("begin running...............")
+
+num_iter = 200
+fwd = 0
+
+with torch.no_grad():
+    t1  = time.time()
+    for i in range(num_iter):
+        y = traced_model(x)
+    t2 = time.time()
+    fwd = fwd + (t2 - t1)
+
+avg_time = fwd / num_iter * 1000
+print("batch_size = %d, avg time is %0.3f (ms) fps:%f"%(batch_size, avg_time, batch_size  * num_iter / fwd))
+
+def trace_handler(prof):
+    print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=-1))
+    # prof.export_chrome_trace("rn50_trace_" + str(prof.step_num) + ".json")
+
+with torch.no_grad():
+    with profiler.profile(
+        activities=[profiler.ProfilerActivity.CPU],
+        schedule=torch.profiler.schedule(wait=10,warmup=50,active=10),
+        # son_trace_ready=torch.profiler.tensorboard_trace_handler("profiler_result")
+        on_trace_ready=trace_handler) as p:
+        for i in range(num_iter):
+            y = traced_model(x)
+            p.step()
---
+++ run_throughput.sh
@@ -0,0 +1,17 @@
+export KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
+
+export DNNL_PRIMITIVE_CACHE_CAPACITY=1024
+
+CORES=`lscpu | grep Core | awk '{print $4}'`
+SOCKETS=`lscpu | grep Socket | awk '{print $2}'`
+TOTAL_CORES=`expr $CORES \* $SOCKETS`
+#BATCH_SIZE=64
+export KMP_BLOCKTIME=1
+export OMP_NUM_THREADS=20
+export $KMP_SETTING
+
+echo -e "### using OMP_NUM_THREADS=$OMP_NUM_THREADS"
+echo -e "### using $KMP_SETTING\n\n"
+sleep 3
+
+numactl --physcpubind=0-19 --membind=0 python -u model_benc.py 
